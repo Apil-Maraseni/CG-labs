d1bc3fce51ef2e92f040a8120d84cf351ec9441e
Apil-Maraseni <apil.maraseni5@gmail.com>
2025-06-18 08:18:04 +0545

    hello

--- Lab03/main.py
+++ Lab03/main.py
@@ -22,20 +22,12 @@ def midpointcircle(x1,y1,x2,y2,r,d):
         else:
             x -= 1
             d = d + 2 * y - 2 * x + 1
-            
-
-
-
-
 
 pygame.init()
 WIDTH = 800
 HEIGHT = 600
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
 pygame.display.set_caption("Midpoint Circle Algorithm Apil Maraseni 012")
-
 
 WHITE = (12, 15, 255)
 BLACK = (255, 255, 255)
